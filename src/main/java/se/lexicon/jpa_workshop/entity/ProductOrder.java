390231db1ff5a9b45dab718aaf1ac79f54574c42
aman adinlid <95pinkpanda@gmail.com>
2021-03-04 16:51:38 +0100

    ##Created ProductOrder class and did unit test for it##

--- src/main/java/se/lexicon/jpa_workshop/entity/ProductOrder.java
+++ src/main/java/se/lexicon/jpa_workshop/entity/ProductOrder.java
@@ -1,14 +1,60 @@
 package se.lexicon.jpa_workshop.entity;
 
-import java.time.LocalDate;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import java.time.LocalDateTime;
 import java.util.Collection;
 
+@Repository
 public class ProductOrder {
     private int id;
     private LocalDateTime orderDateTime;
+    @OneToMany(mappedBy = "productOrder")
     private Collection<OrderItem> orderItems;
-     private AppUser appUser;
+    private AppUser customer;
 
+    public void removeOrderItem(OrderItem orderItem) {
+        orderItem.setProductOrder(null);
+        orderItems.remove(orderItem);
+    }
+
+    public double calculateTotalPrice(Collection<OrderItem> orderItems) {
+        return orderItems.stream().map(x -> x.calculatePrice(x.getProduct(),
+                x.getQuantity())).reduce(0.0, Double::sum);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getOrderDateTime() {
+        return orderDateTime;
+    }
+
+    public void setOrderDateTime(LocalDateTime orderDateTime) {
+        this.orderDateTime = orderDateTime;
+    }
+
+    public Collection<OrderItem> getOrderItems() {
+        return orderItems;
+    }
+
+    public void setOrderItems(Collection<OrderItem> orderItems) {
+        this.orderItems = orderItems;
+    }
+
+    public AppUser getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(AppUser customer) {
+        this.customer = customer;
+    }
 
 }
---
+++ src/test/java/se/lexicon/jpa_workshop/entity/ProductOrderTest.java
@@ -0,0 +1,36 @@
+package se.lexicon.jpa_workshop.entity;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collection;
+
+@SpringBootTest
+public class ProductOrderTest {
+    @Autowired
+    ProductOrder productOrder;
+    Collection<OrderItem> orderItems;
+
+    @Test
+    @DisplayName("Test1: create productOrder")
+    public void test1() {
+        ProductOrder productOrder = new ProductOrder();
+        Collection<OrderItem> orderItems = new ArrayList<>();
+        OrderItem orderItem = new OrderItem();
+        productOrder.setOrderItems(orderItems);
+        productOrder.setOrderDateTime(LocalDateTime.now());
+        productOrder.setCustomer(productOrder.getCustomer());
+        orderItem.setQuantity(20);
+        orderItem.setId(3);
+        orderItem.setProduct(orderItem.getProduct());
+        Assertions.assertEquals(orderItems, productOrder.getOrderItems());
+
+    }
+
+
+}
